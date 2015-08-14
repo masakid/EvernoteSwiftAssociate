34619901ead2f75b942a71590cbd55e05b7c23ee
masakid <runmk827@gmail.com>
2015-08-14 13:25:07 +0900

    first commit Evernote Associate swift

--- EvernoteSwiftAssociate/AppDelegate.swift
+++ EvernoteSwiftAssociate/AppDelegate.swift
@@ -2,8 +2,6 @@
 //  AppDelegate.swift
 //  EvernoteSwiftAssociate
 //
-//  Created by 久保田 将規 on 2015/08/14.
-//  Copyright (c) 2015年 masaki.k. All rights reserved.
 //
 
 import UIKit
---
+++ EvernoteSwiftAssociate/EvernoteSwiftAssociate-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  EvernoteSwiftAssociate-Bridging-Header.h
+//  EvernoteSwiftAssociate
+//
+
+#ifndef EvernoteSwiftAssociate_EvernoteSwiftAssociate_Bridging_Header_h
+#define EvernoteSwiftAssociate_EvernoteSwiftAssociate_Bridging_Header_h
+#import <ENSDK/ENSDK.h>
+
+#endif
