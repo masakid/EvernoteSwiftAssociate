836afa1c65ab49c4cbce994f7737f1638278b6cd
masakid <runmk827@gmail.com>
2015-08-14 12:34:07 +0900

    Initial Commit

---
+++ EvernoteSwiftAssociate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EvernoteSwiftAssociate
+//
+//  Created by 久保田 将規 on 2015/08/14.
+//  Copyright (c) 2015年 masaki.k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
